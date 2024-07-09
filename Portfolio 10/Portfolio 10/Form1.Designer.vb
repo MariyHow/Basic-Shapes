97371ba989dfcbe61c154e77ffad7ecfaedfb419
MariyHow <145704450+MariyHow@users.noreply.github.com>
2024-07-09 01:52:23 -0400

    Add files via upload
    This is the 10th portfolio I created. By running this application, you can turn the background blue by clicking button 2, it is a very simple application.

---
+++ Portfolio 10/Portfolio 10/Form1.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.btnBlue = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Location = New System.Drawing.Point(285, 85)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(94, 29)
+        Me.btnSubmit.TabIndex = 0
+        Me.btnSubmit.Text = "Button1"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'btnBlue
+        '
+        Me.btnBlue.Location = New System.Drawing.Point(334, 183)
+        Me.btnBlue.Name = "btnBlue"
+        Me.btnBlue.Size = New System.Drawing.Size(94, 29)
+        Me.btnBlue.TabIndex = 1
+        Me.btnBlue.Text = "Button2"
+        Me.btnBlue.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnBlue)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnSubmit As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents btnBlue As Button
+End Class
---
+++ Portfolio 10/Portfolio 10/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+    Private Sub btnBlue_Click(sender As Object, e As EventArgs) Handles btnBlue.Click
+        Me.BackColor = Color.Blue
+    End Sub
+End Class
