a50cba3f9df54bbd8f3e3be97e6fc5ce948544a4
MariyHow <145704450+MariyHow@users.noreply.github.com>
2024-07-09 02:15:48 -0400

    Add files via upload
    This is an application I created called Automobile. This application can display the make, year, mileage, and price of a car based on the users input.

---
+++ Automobile/Automobile/Automobile.cpp
@@ -0,0 +1,88 @@
+#pragma once
+// Make
+//Year model
+// Mileage
+// Price
+// A car class attributes for the make, year model, milage, price, and number of doors
+// A Truck class attributes for the make, year model, mileage, price, and drive type
+// A SUV class attributes for the make, year model, mileage, price, and passenger capacity
+
+#include <iostream>
+using namespace std;
+
+class Automobile {
+protected:
+	string make;
+	int year;
+	int mileage;
+	int price;
+public:
+	Automobile() { make = ""; year = 0; mileage = 0; price = 0; }
+	Automobile(string m, int y, int ml, int p) { make = m; year = y; mileage = ml; price = p; }
+	void setMake(string m) { make = m; }
+	void setYear(int y) { year = y; }
+	void setMileage(int ml) { mileage = ml; }
+	void setPrice(int p) { price = p; }
+	string getMake() const { return make; }
+	int getYear() const { return year; }
+	int getMileage() const { return mileage; }
+	int getPrice() const { return price; }
+	void PrintInfo() { cout << make << " " << year << " " << mileage << " " << endl; }
+};
+
+class Car : public Automobile {
+private:
+	int doors;
+public:
+	Car() :Automobile() { doors = 0; }
+	Car(string m, int y, int ml, int p, int d):Automobile(m, y, ml, p) { doors = d; }
+	void setDoors(int d) { doors = d; }
+	int getDoors() const { return doors; }
+
+	void PrintInfo() { cout << make << " " << year << " " << mileage << " " << endl; }
+};
+
+class SUV : public Automobile {
+private:
+	int capacity;
+public:
+	SUV() :Automobile() { capacity = 0; }
+	SUV(string m, int y, int ml, int p, int c):Automobile(m, y, ml, p) { capacity = c; }
+	void setCapacity(int c) { capacity = c; }
+	int getCapacity() const { return capacity; }
+
+	void PrintInfo() { cout << make << " " << year << " " << mileage << " " << price << " " << capacity << " " << endl; }
+};
+
+class Truck : public Automobile {
+protected:
+	string driveType;
+public:
+	Truck() :Automobile() { driveType = ""; }
+	Truck(string m, int y, int ml, int p, string d) : Automobile(m, y, ml, p) { driveType = d; }
+	void setDrive(int d) { driveType = d; }
+	string getDrive() const { return driveType; }
+
+	void PrintInfo() { cout << make << " " << year << " " << mileage << " " << endl; }
+};
+
+class MonsterTruck : public Truck {
+private:
+	int wheelSize;
+public:
+	MonsterTruck() :Truck() { wheelSize = 0; }
+	MonsterTruck(string m, int y, int ml, int p, string d, int w) : Truck(m, y, ml, p, d) { wheelSize = w; }
+	void setWheel(int w) { wheelSize = w; }
+	int getWheel() const { return wheelSize; }
+
+	void PrintInfo() { cout << make << " " << year << " " << mileage << " " << price << " " << driveType << " " << wheelSize << " " << endl; }
+
+};
+
+//----------User Side----------------
+
+int main() {
+	Car C("Toyota", 2019, 10000, 15000, 2); C.PrintInfo();
+
+	MonsterTruck M("Dodge", 2015, 50000, 30000, "AWD", 60); M.PrintInfo();
+}
