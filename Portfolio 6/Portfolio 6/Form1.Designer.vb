bf994edc75e4e03a013583d71355969f9f132d6c
MariyHow <145704450+MariyHow@users.noreply.github.com>
2024-07-09 01:33:00 -0400

    Add files via upload
    This is the 6th portfolio I created. This application is called "Can You Guess the Hidden Number?". As you can guess, you have to guess the number of a randomly generated number by the system.

---
+++ Portfolio 6/Portfolio 6/Form1.Designer.vb
@@ -0,0 +1,75 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(117, 26)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(586, 41)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "CAN YOU GUESS THE HIDDEN NUMBER???"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(101, 106)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(270, 268)
+        Me.Button3.TabIndex = 7
+        Me.Button3.Text = "Reset"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(438, 106)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(265, 268)
+        Me.Button4.TabIndex = 8
+        Me.Button4.Text = "Exit"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label2)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+End Class
---
+++ Portfolio 6/Portfolio 6/Form1.vb
@@ -0,0 +1,31 @@
+﻿Public Class Form1
+    Public num1, num2 As Integer
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Randomize()
+        Dim num1 As Integer = CInt(Int((100 * Rnd()) + 1))
+        Dim num2 As Integer
+        Do
+            num2 = Val(InputBox("Enter a number between 0 and 100"))
+            If (num2 > num1) Then
+                MessageBox.Show("Sorry your number is too high, Try Again.")
+                Me.BackColor = Color.Red
+            ElseIf (num2 < num1) Then
+                MessageBox.Show("Sorry your number is too low, Try Again.")
+                Me.BackColor = Color.Blue
+            ElseIf (num2 = num1) Then
+                MessageBox.Show(num2, "YOU GUESSED THE HIDDEN NUMBER, WERE YOU CHEATING?")
+                Me.BackColor = Color.Green
+            End If
+
+        Loop Until num2 = num1
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        MessageBox.Show("Your program has been reset")
+        Application.Restart()
+    End Sub
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Me.Close()
+    End Sub
+End Class
