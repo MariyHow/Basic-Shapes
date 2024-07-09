57bd135ce025a477ecd7d5273607c00d2b76789a
MariyHow <145704450+MariyHow@users.noreply.github.com>
2024-07-09 01:48:03 -0400

    Add files via upload
    This is the 8th portfolio I created. By running this application, you can display data that was put into a list box. By putting data in a list box you can display it for viewer once you run an application.

---
+++ Portfolio 8/Portfolio 8/Form1.Designer.vb
@@ -0,0 +1,85 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 20
+        Me.ListBox1.Location = New System.Drawing.Point(118, 64)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(201, 284)
+        Me.ListBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(163, 371)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(94, 29)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(486, 371)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(94, 29)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.ItemHeight = 20
+        Me.ListBox2.Location = New System.Drawing.Point(426, 64)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(202, 284)
+        Me.ListBox2.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Name = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ListBox2 As ListBox
+End Class
---
+++ Portfolio 8/Portfolio 8/Form1.vb
@@ -0,0 +1,54 @@
+﻿Imports System.DirectoryServices.ActiveDirectory
+Imports System.IO
+Imports System.Security.Cyptography.Pkcs
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class Form1
+    Public before() As String
+    Public after() As String
+    Dim text() As String = IO.File.ReadAllLines("sampleTextese.txt")
+    Dim paragraph() As String = IO.File.ReadAllLines("sampleparagraph.txt")
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ReDim before(text.Count - 1)
+        ReDim after(text.Count - 1)
+
+        For i = 0 To text.Count - 1
+            ListBox1.Items.Add(text(i))
+            Dim tx = text(i).Split(","c)
+            before(i) = tx(0)
+            after(i) = tx(1)
+            ListBox1.Items.Add(before(i))
+            ListBox1.Items.Add(after(i))
+        Next
+        Dim splitparagraph = paragraph(0).Split(" "c)
+        For p = 0 To splitparagraph.Count - 1
+
+
+
+            Dim ya As Boolean = False
+            Dim ind As Integer = -1
+
+            For t = 0 To before.Count - 1
+                If splitparagraph(p).CompareTo(before(t)) = 0 Then
+                    ya = True
+                    ind = t
+                    t = before.Count
+                Else
+                    ya = False
+                End If
+            Next
+            If ya.Equals(True) Then
+                ListBox2.Items.Add(after(ind))
+            Else
+                ListBox2.Items.Add(splitparagraph(p))
+            End If
+        Next
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+    End Sub
+End Class
