a903207e00483296d830eb0ac4c92829ef51335f
MariyHow <145704450+MariyHow@users.noreply.github.com>
2024-07-09 01:29:20 -0400

    Add files via upload
    This is my Maripot Carpet Store application. By running this application, you can calculate the length and area of a carpet needed for a household.

---
+++ Maripot Carpet Store/Maripot Carpet Store/Form1.Designer.vb
@@ -0,0 +1,279 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnCustomer1 = New System.Windows.Forms.Button()
+        Me.btnCustomer2 = New System.Windows.Forms.Button()
+        Me.btnCustomer3 = New System.Windows.Forms.Button()
+        Me.btnCustomer4 = New System.Windows.Forms.Button()
+        Me.btnReceipt = New System.Windows.Forms.Button()
+        Me.lbReceipt = New System.Windows.Forms.ListBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.btnEnter = New System.Windows.Forms.Button()
+        Me.LabelTitle = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(37, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 20)
+        Me.Label1.TabIndex = 0
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(68, 259)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(0, 20)
+        Me.Label10.TabIndex = 9
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(557, 440)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(94, 29)
+        Me.btnExit.TabIndex = 18
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnCustomer1
+        '
+        Me.btnCustomer1.Location = New System.Drawing.Point(37, 20)
+        Me.btnCustomer1.Name = "btnCustomer1"
+        Me.btnCustomer1.Size = New System.Drawing.Size(94, 73)
+        Me.btnCustomer1.TabIndex = 19
+        Me.btnCustomer1.Text = "1"
+        Me.btnCustomer1.UseVisualStyleBackColor = True
+        '
+        'btnCustomer2
+        '
+        Me.btnCustomer2.Location = New System.Drawing.Point(37, 127)
+        Me.btnCustomer2.Name = "btnCustomer2"
+        Me.btnCustomer2.Size = New System.Drawing.Size(94, 81)
+        Me.btnCustomer2.TabIndex = 20
+        Me.btnCustomer2.Text = "2"
+        Me.btnCustomer2.UseVisualStyleBackColor = True
+        '
+        'btnCustomer3
+        '
+        Me.btnCustomer3.Location = New System.Drawing.Point(37, 241)
+        Me.btnCustomer3.Name = "btnCustomer3"
+        Me.btnCustomer3.Size = New System.Drawing.Size(94, 82)
+        Me.btnCustomer3.TabIndex = 21
+        Me.btnCustomer3.Text = "3"
+        Me.btnCustomer3.UseVisualStyleBackColor = True
+        '
+        'btnCustomer4
+        '
+        Me.btnCustomer4.Location = New System.Drawing.Point(37, 352)
+        Me.btnCustomer4.Name = "btnCustomer4"
+        Me.btnCustomer4.Size = New System.Drawing.Size(94, 81)
+        Me.btnCustomer4.TabIndex = 22
+        Me.btnCustomer4.Text = "4"
+        Me.btnCustomer4.UseVisualStyleBackColor = True
+        '
+        'btnReceipt
+        '
+        Me.btnReceipt.Location = New System.Drawing.Point(444, 440)
+        Me.btnReceipt.Name = "btnReceipt"
+        Me.btnReceipt.Size = New System.Drawing.Size(94, 29)
+        Me.btnReceipt.TabIndex = 23
+        Me.btnReceipt.Text = "Receipt"
+        Me.btnReceipt.UseVisualStyleBackColor = True
+        '
+        'lbReceipt
+        '
+        Me.lbReceipt.FormattingEnabled = True
+        Me.lbReceipt.ItemHeight = 20
+        Me.lbReceipt.Location = New System.Drawing.Point(360, 60)
+        Me.lbReceipt.Name = "lbReceipt"
+        Me.lbReceipt.Size = New System.Drawing.Size(403, 324)
+        Me.lbReceipt.TabIndex = 24
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(175, 43)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(125, 27)
+        Me.TextBox1.TabIndex = 25
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(178, 122)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(125, 27)
+        Me.TextBox2.TabIndex = 26
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(178, 198)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(125, 27)
+        Me.TextBox3.TabIndex = 27
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(175, 273)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(125, 27)
+        Me.TextBox4.TabIndex = 28
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(178, 345)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(125, 27)
+        Me.TextBox5.TabIndex = 29
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(190, 20)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(97, 20)
+        Me.Label2.TabIndex = 30
+        Me.Label2.Text = "Square yards:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(172, 99)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(148, 20)
+        Me.Label3.TabIndex = 31
+        Me.Label3.Text = "Cost per square yard:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(172, 175)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(157, 20)
+        Me.Label4.TabIndex = 32
+        Me.Label4.Text = "Labor per square yard:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(172, 250)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(160, 20)
+        Me.Label5.TabIndex = 33
+        Me.Label5.Text = "Floor Preperation Cost:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(203, 322)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(70, 20)
+        Me.Label6.TabIndex = 34
+        Me.Label6.Text = "Discount:"
+        '
+        'btnEnter
+        '
+        Me.btnEnter.Location = New System.Drawing.Point(506, 390)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(94, 29)
+        Me.btnEnter.TabIndex = 35
+        Me.btnEnter.Text = "Enter"
+        Me.btnEnter.UseVisualStyleBackColor = True
+        '
+        'LabelTitle
+        '
+        Me.LabelTitle.AutoSize = True
+        Me.LabelTitle.Location = New System.Drawing.Point(506, 20)
+        Me.LabelTitle.Name = "LabelTitle"
+        Me.LabelTitle.Size = New System.Drawing.Size(122, 20)
+        Me.LabelTitle.TabIndex = 36
+        Me.LabelTitle.Text = "Customer Recept"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 481)
+        Me.Controls.Add(Me.LabelTitle)
+        Me.Controls.Add(Me.btnEnter)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.lbReceipt)
+        Me.Controls.Add(Me.btnReceipt)
+        Me.Controls.Add(Me.btnCustomer4)
+        Me.Controls.Add(Me.btnCustomer3)
+        Me.Controls.Add(Me.btnCustomer2)
+        Me.Controls.Add(Me.btnCustomer1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "CustomerReceiptHoward"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnCustomer1 As Button
+    Friend WithEvents btnCustomer2 As Button
+    Friend WithEvents btnCustomer3 As Button
+    Friend WithEvents btnCustomer4 As Button
+    Friend WithEvents btnReceipt As Button
+    Friend WithEvents lbReceipt As ListBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents btnEnter As Button
+    Friend WithEvents LabelTitle As Label
+End Class
---
+++ Maripot Carpet Store/Maripot Carpet Store/Form1.vb
@@ -0,0 +1,79 @@
+﻿Imports System.Runtime.ConstrainedExecution
+Imports System.Transactions
+
+Public Class Form1
+    Dim Squareyards As Double = CDbl(TextBox1.Text)
+    Dim Costpersqyard As Double = CDbl(TextBox2.Text)
+    Dim Laborpersqyard As Double = CDbl(TextBox3.Text)
+    Dim Prepcost As Double = CDbl(TextBox4.Text)
+    Dim Discount As Double = CDbl(TextBox5.Text)
+    Private dimReceipt As Object
+
+    Private Sub btnCustomer1_Click(sender As Object, e As EventArgs) Handles btnCustomer1.Click
+        Squareyards = 17
+        Costpersqyard = 18.5
+        Laborpersqyard = 3.5
+        Prepcost = 38.5
+        Discount = 0.02
+    End Sub
+
+    Private Sub btnCustomer2_Click(sender As Object, e As EventArgs) Handles btnCustomer2.Click
+        Squareyards = 40
+        Costpersqyard = 24.95
+        Laborpersqyard = 2.95
+        Prepcost = 0.00
+        Discount = 0.14
+    End Sub
+
+    Private Sub btnCustomer3_Click(sender As Object, e As EventArgs) Handles btnCustomer3.Click
+        Squareyards = 23
+        Costpersqyard = 16.8
+        Laborpersqyard = 3.25
+        Prepcost = 57.95
+        Discount = 0.00
+    End Sub
+
+    Private Sub btnCustomer4_Click(sender As Object, e As EventArgs) Handles btnCustomer4.Click
+        Squareyards = 26
+        Costpersqyard = 21.25
+        Laborpersqyard = 0.00
+        Prepcost = 80.0
+        Discount = 0.02
+    End Sub
+
+    Private Sub btnEnter_Click(sender As Object, e As EventArgs) Handles btnEnter.Click
+        lbReceipt.Items.Clear()
+        CalclbReceipt()
+        Dim a As Double = CDbl(TextBox1.Text)
+        Dim b As Double = CDbl(TextBox1.Text)
+        Dim c As Double = CDbl(TextBox1.Text)
+        Dim d As Double = CDbl(TextBox1.Text)
+        Dim p As Double = CDbl(TextBox1.Text * TextBox3.Text * TextBox4.Text)
+        Dim f As Double = CDbl(TextBox2.Text * TextBox1.Text * 0.02)
+        Dim l As Double = CDbl(TextBox1.Text * TextBox2.Text * 0.04)
+
+        Dim g As Double = CDbl(TextBox5.Text * TextBox1.Text * 0.04)
+
+        lbReceipt.Items.Add("          Maripot Carpet Store           ")
+        lbReceipt.Items.Add("Square Yards Purchased:            $ " & a)
+        lbReceipt.Items.Add("Cost per yard:                     $ " & b)
+        lbReceipt.Items.Add("Labor per square yard:             $ " & c)
+        lbReceipt.Items.Add("Floor prepartation cost:           $ " & d)
+        lbReceipt.Items.Add("Cost for Carpet:                   $ " & p)
+        lbReceipt.Items.Add("Discount on carpet:                $ " & f)
+        lbReceipt.Items.Add("Tax on carpet:                     $ " & g)
+        lbReceipt.Items.Add("Total:                             $" & a + b + c + d + p + f + l + g)
+    End Sub
+
+    Private Sub btnReceipt_Click(sender As Object, e As EventArgs) Handles btnReceipt.Click
+        lbReceipt.Items.Clear()
+    End Sub
+
+    Private Sub Receipt_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lbReceipt.SelectedIndexChanged
+        lbReceipt.Items.Add()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
