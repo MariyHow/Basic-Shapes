c07cdb8e68b8c0b251c3f0665db14e5d3503e8e1
MariyHow <145704450+MariyHow@users.noreply.github.com>
2024-07-09 02:12:35 -0400

    Add files via upload
    This is an application I created called Grade. In this application, you can calculate the grade of a student by adding five different grades together and the application will average them and display the average of the student. Depending on the grade of the average it will display the number, and letter of the grade. Also, it will display a face depending on the grade of the class.

---
+++ Grades/Grades/Form1.Designer.vb
@@ -0,0 +1,441 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.txtAvgGrade = New System.Windows.Forms.TextBox()
+        Me.picBoxF = New System.Windows.Forms.PictureBox()
+        Me.picBoxD = New System.Windows.Forms.PictureBox()
+        Me.picBoxC = New System.Windows.Forms.PictureBox()
+        Me.picBoxB = New System.Windows.Forms.PictureBox()
+        Me.picBoxA = New System.Windows.Forms.PictureBox()
+        Me.LabelF = New System.Windows.Forms.Label()
+        Me.LabelD = New System.Windows.Forms.Label()
+        Me.LabelC = New System.Windows.Forms.Label()
+        Me.LabelB = New System.Windows.Forms.Label()
+        Me.LabelA = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.LabelAvgGrade = New System.Windows.Forms.Label()
+        Me.txtGrade1 = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.txtGrade2 = New System.Windows.Forms.TextBox()
+        Me.txtGrade3 = New System.Windows.Forms.TextBox()
+        Me.txtGrade4 = New System.Windows.Forms.TextBox()
+        Me.txtGrade5 = New System.Windows.Forms.TextBox()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnEnter = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        CType(Me.picBoxF, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picBoxD, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picBoxC, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picBoxB, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picBoxA, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnReset
+        '
+        Me.btnReset.Location = New System.Drawing.Point(656, 367)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(94, 29)
+        Me.btnReset.TabIndex = 0
+        Me.btnReset.Text = "Reset"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'txtAvgGrade
+        '
+        Me.txtAvgGrade.Location = New System.Drawing.Point(494, 251)
+        Me.txtAvgGrade.Name = "txtAvgGrade"
+        Me.txtAvgGrade.Size = New System.Drawing.Size(125, 27)
+        Me.txtAvgGrade.TabIndex = 1
+        '
+        'picBoxF
+        '
+        Me.picBoxF.Image = Global.Grades.My.Resources.Resources.F_sad_face
+        Me.picBoxF.Location = New System.Drawing.Point(24, 69)
+        Me.picBoxF.Name = "picBoxF"
+        Me.picBoxF.Size = New System.Drawing.Size(125, 131)
+        Me.picBoxF.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picBoxF.TabIndex = 2
+        Me.picBoxF.TabStop = False
+        '
+        'picBoxD
+        '
+        Me.picBoxD.Image = Global.Grades.My.Resources.Resources.D_sad_face
+        Me.picBoxD.Location = New System.Drawing.Point(172, 69)
+        Me.picBoxD.Name = "picBoxD"
+        Me.picBoxD.Size = New System.Drawing.Size(125, 131)
+        Me.picBoxD.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picBoxD.TabIndex = 3
+        Me.picBoxD.TabStop = False
+        '
+        'picBoxC
+        '
+        Me.picBoxC.Image = Global.Grades.My.Resources.Resources.C_okay_face
+        Me.picBoxC.Location = New System.Drawing.Point(335, 69)
+        Me.picBoxC.Name = "picBoxC"
+        Me.picBoxC.Size = New System.Drawing.Size(125, 131)
+        Me.picBoxC.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picBoxC.TabIndex = 4
+        Me.picBoxC.TabStop = False
+        '
+        'picBoxB
+        '
+        Me.picBoxB.Image = Global.Grades.My.Resources.Resources.B_happy_face
+        Me.picBoxB.Location = New System.Drawing.Point(489, 69)
+        Me.picBoxB.Name = "picBoxB"
+        Me.picBoxB.Size = New System.Drawing.Size(125, 131)
+        Me.picBoxB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picBoxB.TabIndex = 5
+        Me.picBoxB.TabStop = False
+        '
+        'picBoxA
+        '
+        Me.picBoxA.Image = Global.Grades.My.Resources.Resources.A_grade_happy_face
+        Me.picBoxA.Location = New System.Drawing.Point(638, 69)
+        Me.picBoxA.Name = "picBoxA"
+        Me.picBoxA.Size = New System.Drawing.Size(125, 131)
+        Me.picBoxA.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picBoxA.TabIndex = 6
+        Me.picBoxA.TabStop = False
+        '
+        'LabelF
+        '
+        Me.LabelF.AutoSize = True
+        Me.LabelF.Location = New System.Drawing.Point(80, 33)
+        Me.LabelF.Name = "LabelF"
+        Me.LabelF.Size = New System.Drawing.Size(16, 20)
+        Me.LabelF.TabIndex = 7
+        Me.LabelF.Text = "F"
+        '
+        'LabelD
+        '
+        Me.LabelD.AutoSize = True
+        Me.LabelD.Location = New System.Drawing.Point(221, 33)
+        Me.LabelD.Name = "LabelD"
+        Me.LabelD.Size = New System.Drawing.Size(20, 20)
+        Me.LabelD.TabIndex = 8
+        Me.LabelD.Text = "D"
+        '
+        'LabelC
+        '
+        Me.LabelC.AutoSize = True
+        Me.LabelC.Location = New System.Drawing.Point(390, 33)
+        Me.LabelC.Name = "LabelC"
+        Me.LabelC.Size = New System.Drawing.Size(18, 20)
+        Me.LabelC.TabIndex = 9
+        Me.LabelC.Text = "C"
+        '
+        'LabelB
+        '
+        Me.LabelB.AutoSize = True
+        Me.LabelB.Location = New System.Drawing.Point(535, 33)
+        Me.LabelB.Name = "LabelB"
+        Me.LabelB.Size = New System.Drawing.Size(18, 20)
+        Me.LabelB.TabIndex = 10
+        Me.LabelB.Text = "B"
+        '
+        'LabelA
+        '
+        Me.LabelA.AutoSize = True
+        Me.LabelA.Location = New System.Drawing.Point(688, 33)
+        Me.LabelA.Name = "LabelA"
+        Me.LabelA.Size = New System.Drawing.Size(19, 20)
+        Me.LabelA.TabIndex = 11
+        Me.LabelA.Text = "A"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(62, 220)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(64, 20)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "Grade 1:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(62, 259)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(64, 20)
+        Me.Label7.TabIndex = 13
+        Me.Label7.Text = "Grade 2:"
+        '
+        'LabelAvgGrade
+        '
+        Me.LabelAvgGrade.AutoSize = True
+        Me.LabelAvgGrade.Location = New System.Drawing.Point(335, 254)
+        Me.LabelAvgGrade.Name = "LabelAvgGrade"
+        Me.LabelAvgGrade.Size = New System.Drawing.Size(153, 20)
+        Me.LabelAvgGrade.TabIndex = 14
+        Me.LabelAvgGrade.Text = "Average Grade Letter:"
+        '
+        'txtGrade1
+        '
+        Me.txtGrade1.Location = New System.Drawing.Point(145, 220)
+        Me.txtGrade1.Name = "txtGrade1"
+        Me.txtGrade1.Size = New System.Drawing.Size(125, 27)
+        Me.txtGrade1.TabIndex = 15
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(62, 301)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(64, 20)
+        Me.Label9.TabIndex = 16
+        Me.Label9.Text = "Grade 3:"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(62, 338)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(64, 20)
+        Me.Label10.TabIndex = 17
+        Me.Label10.Text = "Grade 4:"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(62, 376)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(64, 20)
+        Me.Label11.TabIndex = 18
+        Me.Label11.Text = "Grade 5:"
+        '
+        'txtGrade2
+        '
+        Me.txtGrade2.Location = New System.Drawing.Point(145, 256)
+        Me.txtGrade2.Name = "txtGrade2"
+        Me.txtGrade2.Size = New System.Drawing.Size(125, 27)
+        Me.txtGrade2.TabIndex = 19
+        '
+        'txtGrade3
+        '
+        Me.txtGrade3.Location = New System.Drawing.Point(145, 298)
+        Me.txtGrade3.Name = "txtGrade3"
+        Me.txtGrade3.Size = New System.Drawing.Size(125, 27)
+        Me.txtGrade3.TabIndex = 20
+        '
+        'txtGrade4
+        '
+        Me.txtGrade4.Location = New System.Drawing.Point(145, 331)
+        Me.txtGrade4.Name = "txtGrade4"
+        Me.txtGrade4.Size = New System.Drawing.Size(125, 27)
+        Me.txtGrade4.TabIndex = 21
+        '
+        'txtGrade5
+        '
+        Me.txtGrade5.Location = New System.Drawing.Point(145, 371)
+        Me.txtGrade5.Name = "txtGrade5"
+        Me.txtGrade5.Size = New System.Drawing.Size(125, 27)
+        Me.txtGrade5.TabIndex = 22
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(656, 402)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(94, 29)
+        Me.btnExit.TabIndex = 23
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnEnter
+        '
+        Me.btnEnter.Location = New System.Drawing.Point(160, 409)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(94, 29)
+        Me.btnEnter.TabIndex = 24
+        Me.btnEnter.Text = "Enter"
+        Me.btnEnter.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(494, 223)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(53, 20)
+        Me.Label1.TabIndex = 25
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(377, 223)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(111, 20)
+        Me.Label2.TabIndex = 26
+        Me.Label2.Text = "Average Grade:"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Grades.My.Resources.Resources.Fantastic
+        Me.PictureBox1.Location = New System.Drawing.Point(24, 33)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(739, 386)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 27
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = Global.Grades.My.Resources.Resources.Good_Job
+        Me.PictureBox2.Location = New System.Drawing.Point(24, 33)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(739, 386)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 28
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = Global.Grades.My.Resources.Resources.Okay
+        Me.PictureBox3.Location = New System.Drawing.Point(24, 33)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(739, 386)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 29
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Image = Global.Grades.My.Resources.Resources.Bad
+        Me.PictureBox4.Location = New System.Drawing.Point(24, 33)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(739, 386)
+        Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox4.TabIndex = 30
+        Me.PictureBox4.TabStop = False
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.Image = Global.Grades.My.Resources.Resources.Fail
+        Me.PictureBox5.Location = New System.Drawing.Point(24, 33)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(739, 386)
+        Me.PictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox5.TabIndex = 31
+        Me.PictureBox5.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnEnter)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.txtGrade5)
+        Me.Controls.Add(Me.txtGrade4)
+        Me.Controls.Add(Me.txtGrade3)
+        Me.Controls.Add(Me.txtGrade2)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.txtGrade1)
+        Me.Controls.Add(Me.LabelAvgGrade)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.LabelA)
+        Me.Controls.Add(Me.LabelB)
+        Me.Controls.Add(Me.LabelC)
+        Me.Controls.Add(Me.LabelD)
+        Me.Controls.Add(Me.LabelF)
+        Me.Controls.Add(Me.picBoxA)
+        Me.Controls.Add(Me.picBoxB)
+        Me.Controls.Add(Me.picBoxC)
+        Me.Controls.Add(Me.picBoxD)
+        Me.Controls.Add(Me.picBoxF)
+        Me.Controls.Add(Me.txtAvgGrade)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox5)
+        Me.Name = "Form1"
+        Me.Text = "GradesHoward"
+        CType(Me.picBoxF, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picBoxD, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picBoxC, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picBoxB, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picBoxA, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnReset As Button
+    Friend WithEvents txtAvgGrade As TextBox
+    Friend WithEvents picBoxF As PictureBox
+    Friend WithEvents picBoxD As PictureBox
+    Friend WithEvents picBoxC As PictureBox
+    Friend WithEvents picBoxB As PictureBox
+    Friend WithEvents picBoxA As PictureBox
+    Friend WithEvents LabelF As Label
+    Friend WithEvents LabelD As Label
+    Friend WithEvents LabelC As Label
+    Friend WithEvents LabelB As Label
+    Friend WithEvents LabelA As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents LabelAvgGrade As Label
+    Friend WithEvents txtGrade1 As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents txtGrade2 As TextBox
+    Friend WithEvents txtGrade3 As TextBox
+    Friend WithEvents txtGrade4 As TextBox
+    Friend WithEvents txtGrade5 As TextBox
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnEnter As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents PictureBox5 As PictureBox
+End Class
---
+++ Grades/Grades/Form1.vb
@@ -0,0 +1,94 @@
+﻿Public Class Form1
+
+    Private Sub btnEnter_Click(sender As Object, e As EventArgs) Handles btnEnter.Click
+        Dim Grade1 As Double = CDbl(txtGrade1.Text)
+        Dim Grade2 As Double = CDbl(txtGrade2.Text)
+        Dim Grade3 As Double = CDbl(txtGrade3.Text)
+        Dim Grade4 As Double = CDbl(txtGrade4.Text)
+        Dim Grade5 As Double = CDbl(txtGrade5.Text)
+
+        Dim AvgGrade = (Grade1 + Grade2 + Grade3 + Grade4 + Grade5) / 5
+        Label1.Text = AvgGrade
+
+        If (AvgGrade >= 90) Then
+            txtAvgGrade.Text = "A"
+            picBoxA.Visible = True
+            picBoxB.Visible = False
+            picBoxC.Visible = False
+            picBoxD.Visible = False
+            picBoxF.Visible = False
+            PictureBox1.Visible = True
+            PictureBox2.Visible = False
+            PictureBox3.Visible = False
+            PictureBox4.Visible = False
+            PictureBox5.Visible = False
+            Me.BackColor = Color.Aqua
+        ElseIf (AvgGrade >= 80) Then
+            txtAvgGrade.Text = "B"
+            picBoxA.Visible = False
+            picBoxB.Visible = True
+            picBoxC.Visible = False
+            picBoxD.Visible = False
+            picBoxF.Visible = False
+            PictureBox1.Visible = False
+            PictureBox2.Visible = True
+            PictureBox3.Visible = False
+            PictureBox4.Visible = False
+            PictureBox5.Visible = False
+            Me.BackColor = Color.Green
+        ElseIf (AvgGrade >= 70) Then
+            txtAvgGrade.Text = "C"
+            picBoxA.Visible = False
+            picBoxB.Visible = False
+            picBoxC.Visible = True
+            picBoxD.Visible = False
+            picBoxF.Visible = False
+            PictureBox1.Visible = False
+            PictureBox2.Visible = False
+            PictureBox3.Visible = True
+            PictureBox4.Visible = False
+            PictureBox5.Visible = False
+            Me.BackColor = Color.Yellow
+        ElseIf (AvgGrade > 60) Then
+            txtAvgGrade.Text = "D"
+            picBoxA.Visible = False
+            picBoxB.Visible = False
+            picBoxC.Visible = False
+            picBoxD.Visible = True
+            picBoxF.Visible = False
+            PictureBox1.Visible = False
+            PictureBox2.Visible = False
+            PictureBox3.Visible = False
+            PictureBox4.Visible = True
+            PictureBox5.Visible = False
+            Me.BackColor = Color.Orange
+        ElseIf (AvgGrade >= 0) Then
+            txtAvgGrade.Text = "F"
+            picBoxA.Visible = False
+            picBoxB.Visible = False
+            picBoxC.Visible = False
+            picBoxD.Visible = False
+            picBoxF.Visible = True
+            PictureBox1.Visible = False
+            PictureBox2.Visible = False
+            PictureBox3.Visible = False
+            PictureBox4.Visible = False
+            PictureBox5.Visible = True
+            Me.BackColor = Color.Red
+        End If
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        MessageBox.Show("Your program has been reset")
+        Application.Restart()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
