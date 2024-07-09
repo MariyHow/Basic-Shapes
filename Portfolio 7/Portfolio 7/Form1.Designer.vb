707dc7048bb634e84d228e70a1c60dbf0d1f82cb
MariyHow <145704450+MariyHow@users.noreply.github.com>
2024-07-09 01:39:33 -0400

    Add files via upload
    This is the 7th portfolio I created. By running this application, you can look up information about an employee by typing in the employee's number ID. The employees are the past presidents of the United States.

---
+++ Portfolio 7/Portfolio 7/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 20
+        Me.ListBox1.Location = New System.Drawing.Point(55, 37)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(293, 384)
+        Me.ListBox1.TabIndex = 0
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(418, 37)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(345, 374)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Search Employee"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button2 As Button
+End Class
---
+++ Portfolio 7/Portfolio 7/Form2.vb
@@ -0,0 +1,30 @@
+﻿Imports System.Runtime
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Public Class Form2
+    Public employeeinfo() = Form1.stemployee
+    Public employeenum(employeeinfo.Length)
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim ln(employeeinfo.Length)
+        Dim fn(employeeinfo.Length)
+
+        For i = 0 To employeeinfo.Length - 1
+            Dim line = employeeinfo(i).Split(","c)
+            ln(i) = (line(0))
+            fn(i) = (line(1))
+            employeenum(i) = (line(2))
+            DataGridView1.Rows.Add(ln(i), fn(i), employeenum(i))
+        Next
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim userInput As String = TextBox1.Text
+        Dim numVar As Integer = 0
+        numVar = Array.IndexOf(employeenum, userInput)
+        If numVar <> -1 Then
+            TextBox1.Text = employeeinfo(numVar)
+        Else
+            TextBox1.Text = "Employee Not found" & numVar
+        End If
+    End Sub
+
+End Class
