db2561c4b077cb5c6f9a288fa91398d2657d30a6
MariyHow <145704450+MariyHow@users.noreply.github.com>
2024-07-09 01:56:17 -0400

    Add files via upload
    This is the 9th application I created. By running this application, you can guess the username and password of the application. If your guess is correct, it will gain you access, however if you are wrong it will ask you to guess again.

---
+++ Portfolio 9/Portfolio 9/Form1.vb
@@ -0,0 +1,32 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Public username As String = userinput("Enter username:")
+    Public password As String = userinput2("Enter password:")
+    Public userinput As String
+    Public userinput2 As String
+    Public Grocerylist As New ArrayList
+    Dim list() As String = IO.File.ReadAllLines("Grocerylist.txt")
+    Dim combined As String = username & password
+    Dim codeArray(,) As String = {{username, password}, {username, password}}
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        username = "VisualBasics"
+        password = "needatleastaB"
+        Do
+            userinput = InputBox("What is your username?")
+            If (userinput <> username) Then
+                MessageBox.Show("Sorry that is the wrong username")
+            ElseIf (userinput = username) Then
+                Print("What is the password?")
+            End If
+        Loop Until userinput = username
+
+        Do
+            If (userinput2 = password) Then
+                MessageBox.Show("Access Granted!")
+            ElseIf (userinput2 <> password) Then
+                MessageBox.Show("Sorry that is the wrong password")
+            End If
+        Loop Until userinput2 = password
+    End Sub
+End Class
